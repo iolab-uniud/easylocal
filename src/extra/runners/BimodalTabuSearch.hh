c7148d030bd75a499952e9ce4bfbf6643eefef3a
Andrea Schaerf <schaerf@uniud.it>
2008-05-05 13:45:32 +0000

    Added BimodalRunnerObserver + minor bug-fix.
    git-svn-id: file:///mnt/stora_backup/satt/backup-before-new-install/var/svn/repos/software/EasyLocalpp/trunk@125 1ceef749-5ca8-4b7e-9fa7-ff5341897c31

--- src/extra/runners/BimodalTabuSearch.hh
+++ src/extra/runners/BimodalTabuSearch.hh
@@ -6,7 +6,7 @@
 
 template <class Input, class State, class Move1, class Move2, typename CFtype = int>
 class BimodalTabuSearch
-: public BimodalMoveRunner<Input,State, Move1, Move2,CFtype>
+  : public BimodalMoveRunner<Input,State, Move1, Move2,CFtype>
 {
 public:
   void Print(std::ostream& os = std::cout) const;
@@ -41,26 +41,26 @@ protected:
 };
 
 /*************************************************************************
-* Implementation
-*************************************************************************/
+ * Implementation
+ *************************************************************************/
 
 /**
-Constructs a tabu search runner by linking it to a state manager, 
- a neighborhood explorer, a tabu list manager, and an input object.
+   Constructs a tabu search runner by linking it to a state manager, 
+   a neighborhood explorer, a tabu list manager, and an input object.
  
- @param s a pointer to a compatible state manager
- @param ne a pointer to a compatible neighborhood explorer
- @param tlm a pointer to a compatible tabu list manager
- @param in a poiter to an input object
- */
+   @param s a pointer to a compatible state manager
+   @param ne a pointer to a compatible neighborhood explorer
+   @param tlm a pointer to a compatible tabu list manager
+   @param in a poiter to an input object
+*/
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::BimodalTabuSearch(const Input& in,
-							      StateManager<Input,State,CFtype>& sm,
-							      NeighborhoodExplorer<Input,State,Move1,CFtype>& ne1,
-							      NeighborhoodExplorer<Input,State,Move2,CFtype>& ne2,
-							      TabuListManager<State, Move1,CFtype>& tlm1,
-							      TabuListManager<State, Move2,CFtype>& tlm2,
-							      std::string name)
+								     StateManager<Input,State,CFtype>& sm,
+								     NeighborhoodExplorer<Input,State,Move1,CFtype>& ne1,
+								     NeighborhoodExplorer<Input,State,Move2,CFtype>& ne2,
+								     TabuListManager<State, Move1,CFtype>& tlm1,
+								     TabuListManager<State, Move2,CFtype>& tlm2,
+								     std::string name)
   : BimodalMoveRunner<Input,State,Move1,Move2,CFtype>(in, sm, ne1, ne2, name), pm1(tlm1), pm2(tlm2), max_idle_iteration(0)
 {}
 
@@ -97,9 +97,9 @@ void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::GoCheck() const
 
 
 /**
-Selects always the best move that is non prohibited by the tabu list 
- mechanism.
- */
+   Selects always the best move that is non prohibited by the tabu list 
+   mechanism.
+*/
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::SelectMove()
 {
@@ -117,161 +117,153 @@ void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::SelectMove()
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::SelectMove1() 
 {
-	Move1 mv;
-	register CFtype mv_cost;
-	register bool all_moves_tabu = true;
+  Move1 mv;
+  register CFtype mv_cost;
+  register bool all_moves_tabu = true;
 	
-	this->ne1.FirstMove(this->current_state, mv);
-	mv_cost = this->ne1.DeltaCostFunction(this->current_state, mv);
-	Move1 best_move = mv;
-	CFtype best_delta = mv_cost;
-	do  // look for the best non prohibited move
-	{   // (if all moves are prohibited, then get the best)
-			// For efficency, ProhibitedMove is invoked only when strictly necessary
-		if (mv_cost < best_delta)
-		{
-			if (!pm1.ProhibitedMove(this->current_state, mv, mv_cost))
-			{
-				best_move = mv;
-				best_delta = mv_cost;
-				all_moves_tabu = false;
-			}
-			if (all_moves_tabu)
-			{
-				best_move = mv;
-				best_delta = mv_cost;
-			}
-		}
-		else if (all_moves_tabu && !pm1.ProhibitedMove(this->current_state, mv, mv_cost))
-		{ // even though it is not an improving move,
-			// this move is the actual best since it's the first non-tabu
-			best_move = mv;
-			best_delta = mv_cost;
-			all_moves_tabu = false;
-		}
-		this->ne1.NextMove(this->current_state, mv);
-		mv_cost = this->ne1.DeltaCostFunction(this->current_state, mv);
+  this->ne1.FirstMove(this->current_state, mv);
+  mv_cost = this->ne1.DeltaCostFunction(this->current_state, mv);
+  Move1 best_move = mv;
+  CFtype best_delta = mv_cost;
+  do  // look for the best non prohibited move
+    {   // (if all moves are prohibited, then get the best)
+      // For efficency, ProhibitedMove is invoked only when strictly necessary
+      if (mv_cost < best_delta)
+	{
+	  if (!pm1.ProhibitedMove(this->current_state, mv, mv_cost))
+	    {
+	      best_move = mv;
+	      best_delta = mv_cost;
+	      all_moves_tabu = false;
+	    }
+	  if (all_moves_tabu)
+	    {
+	      best_move = mv;
+	      best_delta = mv_cost;
+	    }
 	}
-	while (!this->ne1.LastMoveDone(this->current_state, mv));
+      else if (all_moves_tabu && !pm1.ProhibitedMove(this->current_state, mv, mv_cost))
+	{ // even though it is not an improving move,
+	  // this move is the actual best since it's the first non-tabu
+	  best_move = mv;
+	  best_delta = mv_cost;
+	  all_moves_tabu = false;
+	}
+      this->ne1.NextMove(this->current_state, mv);
+      mv_cost = this->ne1.DeltaCostFunction(this->current_state, mv);
+    }
+  while (!this->ne1.LastMoveDone(this->current_state, mv));
 	
-	this->current_move1 = best_move;
-	this->current_move_cost1 = best_delta;
+  this->current_move1 = best_move;
+  this->current_move_cost1 = best_delta;
 }
 
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::SelectMove2() 
 {
-	Move2 mv;
-	register CFtype mv_cost;
-	register bool all_moves_tabu = true;
+  Move2 mv;
+  register CFtype mv_cost;
+  register bool all_moves_tabu = true;
 	
-	this->ne2.FirstMove(this->current_state, mv);
-	mv_cost = this->ne2.DeltaCostFunction(this->current_state, mv);
-	Move2 best_move = mv;
-	CFtype best_delta = mv_cost;
-	do  // look for the best non prohibited move
-	{   // (if all moves are prohibited, then get the best)
-			// For efficency, ProhibitedMove is invoked only when strictly necessary
-		if (mv_cost < best_delta)
-		{
-			if (!pm2.ProhibitedMove(this->current_state, mv, mv_cost))
-			{
-				best_move = mv;
-				best_delta = mv_cost;
-				all_moves_tabu = false;
-			}
-			if (all_moves_tabu)
-			{
-				best_move = mv;
-				best_delta = mv_cost;
-			}
-		}
-		else if (all_moves_tabu && !pm2.ProhibitedMove(this->current_state, mv, mv_cost))
-		{ // even though it is not an improving move,
-			// this move is the actual best since it's the first non-tabu
-			best_move = mv;
-			best_delta = mv_cost;
-			all_moves_tabu = false;
-		}
-		this->ne2.NextMove(this->current_state, mv);
-		mv_cost = this->ne2.DeltaCostFunction(this->current_state, mv);
+  this->ne2.FirstMove(this->current_state, mv);
+  mv_cost = this->ne2.DeltaCostFunction(this->current_state, mv);
+  Move2 best_move = mv;
+  CFtype best_delta = mv_cost;
+  do  // look for the best non prohibited move
+    {   // (if all moves are prohibited, then get the best)
+      // For efficency, ProhibitedMove is invoked only when strictly necessary
+      if (mv_cost < best_delta)
+	{
+	  if (!pm2.ProhibitedMove(this->current_state, mv, mv_cost))
+	    {
+	      best_move = mv;
+	      best_delta = mv_cost;
+	      all_moves_tabu = false;
+	    }
+	  if (all_moves_tabu)
+	    {
+	      best_move = mv;
+	      best_delta = mv_cost;
+	    }
 	}
-	while (!this->ne2.LastMoveDone(this->current_state, mv));
+      else if (all_moves_tabu && !pm2.ProhibitedMove(this->current_state, mv, mv_cost))
+	{ // even though it is not an improving move,
+	  // this move is the actual best since it's the first non-tabu
+	  best_move = mv;
+	  best_delta = mv_cost;
+	  all_moves_tabu = false;
+	}
+      this->ne2.NextMove(this->current_state, mv);
+      mv_cost = this->ne2.DeltaCostFunction(this->current_state, mv);
+    }
+  while (!this->ne2.LastMoveDone(this->current_state, mv));
 	
-	this->current_move2 = best_move;
-	this->current_move_cost2 = best_delta;
+  this->current_move2 = best_move;
+  this->current_move_cost2 = best_delta;
 }
 
 /**
-The stop criterion is based on the number of iterations elapsed from
- the last strict improvement of the best state cost.
- */
+   The stop criterion is based on the number of iterations elapsed from
+   the last strict improvement of the best state cost.
+*/
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 bool BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::StopCriterion()
 { return this->number_of_iterations - this->iteration_of_best >= this->max_idle_iteration; }
 
 /**
-In tabu search the selected move is always accepted.
- That is, the acceptability test is replaced by the 
- prohibition mechanism which is managed inside the selection.
- */
+   In tabu search the selected move is always accepted.
+   That is, the acceptability test is replaced by the 
+   prohibition mechanism which is managed inside the selection.
+*/
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 bool BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::AcceptableMove()
 { return true; }
 
 
 /**
-Stores the move by inserting it in the tabu list, if the state obtained
- is better than the one found so far also the best state is updated.
- */
+   Stores the move by inserting it in the tabu list, if the state obtained
+   is better than the one found so far also the best state is updated.
+*/
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::StoreMove()
 {
-#if VERBOSE >= 3
+  if (this->observer != NULL)
+    this->observer->NotifyStoreMove(*this);
   if (this->current_move_type == MOVE_1)
-    std::cerr << "Move (1) : " << this->current_move1 << ", Cost:" << this->current_move_cost1 << " (current: "
-	    << this->current_state_cost << ", best: " << this->best_state_cost << "), it: " << this->number_of_iterations
-	      << " (idle: " << this->number_of_iterations - this->iteration_of_best << ")" 
-	     << std::endl;
+    {
+      this->pm1.InsertMove(this->current_state, this->current_move1, this->current_move_cost1, this->current_state_cost, this->best_state_cost);
+      this->pm2.UpdateIteration();
+    }
   else
-    std::cerr << "Move (2) : " << this->current_move2 << ", Cost:" << this->current_move_cost2 << " (current: "
-	    << this->current_state_cost << ", best: " << this->best_state_cost << "), it: " << this->number_of_iterations
-	      << " (idle: " << this->number_of_iterations - this->iteration_of_best << ")" 
-	      << std::endl;    
-#endif
-	if (this->current_move_type == MOVE_1)
-	{
-		this->pm1.InsertMove(this->current_state, this->current_move1, this->current_move_cost1, this->current_state_cost, this->best_state_cost);
-		this->pm2.UpdateIteration();
-	}
-	else
-	{
-		this->pm2.InsertMove(this->current_state, this->current_move2, this->current_move_cost2, this->current_state_cost, this->best_state_cost);
-		this->pm1.UpdateIteration();
-	}
-	if (LessThan(this->current_state_cost,this->best_state_cost))
-	{
-		this->iteration_of_best = this->number_of_iterations;
-		this->best_state = this->current_state;
-		this->best_state_cost = this->current_state_cost;
-	}
+    {
+      this->pm2.InsertMove(this->current_state, this->current_move2, this->current_move_cost2, this->current_state_cost, this->best_state_cost);
+      this->pm1.UpdateIteration();
+    }
+  if (LessThan(this->current_state_cost,this->best_state_cost))
+    {
+      if (this->observer != NULL)
+	this->observer->NotifyNewBest(*this);
+      this->iteration_of_best = this->number_of_iterations;
+      this->best_state = this->current_state;
+      this->best_state_cost = this->current_state_cost;
+    }
 }
 
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::TerminateRun()
 {
-	BimodalMoveRunner<Input,State,Move1,Move2,CFtype>::TerminateRun();
+  BimodalMoveRunner<Input,State,Move1,Move2,CFtype>::TerminateRun();
 }
 
 template <class Input, class State, class Move1, class Move2, typename CFtype>
 void BimodalTabuSearch<Input,State,Move1,Move2,CFtype>::ReadParameters(std::istream& is, std::ostream& os)
 {
-	os << "BIMODAL TABU SEARCH -- INPUT PARAMETERS" << std::endl;
-	pm1.ReadParameters(is,os);
-	pm2.ReadParameters(is,os);
-	os << "  Number of idle iterations: ";
-	is >> this->max_idle_iteration;
-// 	os << "  Timeout: ";
-// 	is >> this->timeout;
+  os << "BIMODAL TABU SEARCH -- INPUT PARAMETERS" << std::endl;
+  pm1.ReadParameters(is,os);
+  pm2.ReadParameters(is,os);
+  os << "  Number of idle iterations: ";
+  is >> this->max_idle_iteration;
+  // 	os << "  Timeout: ";
+  // 	is >> this->timeout;
 }
 #endif /*BIMODALTABUSEARCH_HH_*/
