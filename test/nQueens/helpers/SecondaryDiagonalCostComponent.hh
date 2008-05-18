d970c2b98cd34575a3bfdc8699c5f3ba421b1d50
Luca Di Gaspero <luca.digaspero@uniud.it>
2008-05-18 19:01:55 +0000

    Now adding license info to nQueens (first block).
    git-svn-id: file:///mnt/stora_backup/satt/backup-before-new-install/var/svn/repos/software/EasyLocalpp/tags/2.0-pre@196 1ceef749-5ca8-4b7e-9fa7-ff5341897c31

--- test/nQueens/helpers/SecondaryDiagonalCostComponent.hh
+++ test/nQueens/helpers/SecondaryDiagonalCostComponent.hh
@@ -1,3 +1,21 @@
+// $Id$
+// This file is part of EasyLocalpp: a C++ Object-Oriented framework
+// aimed at easing the development of Local Search algorithms.
+// Copyright (C) 2001--2008 Andrea Schaerf, Luca Di Gaspero. 
+//
+// EasyLocalpp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// EasyLocalpp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with EasyLocalpp. If not, see <http://www.gnu.org/licenses/>.
+
 #if !defined(_SECONDARYDIAGONALCOSTCOMPONENT_HH_)
 #define _SECONDARYDIAGONALCOSTCOMPONENT_HH_
 
