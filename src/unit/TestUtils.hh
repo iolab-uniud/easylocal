4d8d3eecbd505169e5dfd63a84cb1e7abe176a0f
Luca Di Gaspero <luca.digaspero@uniud.it>
2008-05-18 18:59:30 +0000

    The unit testing classes are ok.
    git-svn-id: file:///mnt/stora_backup/satt/backup-before-new-install/var/svn/repos/software/EasyLocalpp/tags/2.0-pre@195 1ceef749-5ca8-4b7e-9fa7-ff5341897c31

--- src/unit/TestUtils.hh
+++ src/unit/TestUtils.hh
@@ -1,11 +1,23 @@
-/*
- *  Utils.hh
- *  EasyLocalpp
- *
- *  Created by Luca Di Gaspero on 25/04/08.
- *  Copyright 2008 Università degli Studi di Udine. All rights reserved.
- *
- */
+// $Id$
+// This file is part of EasyLocalpp: a C++ Object-Oriented framework
+// aimed at easing the development of Local Search algorithms.
+// Copyright (C) 2001--2008 Andrea Schaerf, Luca Di Gaspero. 
+//
+// EasyLocalpp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// EasyLocalpp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with EasyLocalpp. If not, see <http://www.gnu.org/licenses/>.
+
+#if !defined(_TESTUTILS_HH_)
+#define _TESTUTILS_HH_
 
 #include <string>
 #include <sstream>
@@ -21,3 +33,6 @@ inline std::string stringify(const char* message, const char* file, unsigned int
 	
 	return s.str();
 }
+
+#endif //_TESTUTILS_HH_
+
