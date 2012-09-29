c3a11cb2d57498e2fa5f2072cadf4dc48ca274d8
Luca Di Gaspero <l.digaspero@uniud.it>
2012-09-29 13:00:04 +0200

    moved compiled part to cc

--- src/utils/Parameter.cc
+++ src/utils/Parameter.cc
@@ -1,3 +1,8 @@
 #include "Parameter.hh"
 
 std::vector<const ParameterBox*> ParameterBox::overall_parameters;
+
+const char* ParameterNotSet::what() const throw()
+{
+  return (std::string("Parameter ") + p_par->cmdline_flag + " not set").c_str();
+}
--- src/utils/Parameter.hh
+++ src/utils/Parameter.hh
@@ -35,11 +35,6 @@ protected:
   const AbstractParameter* p_par;
 };
 
-const char* ParameterNotSet::what() const throw()
-{
-  return (std::string("Parameter ") + p_par->cmdline_flag + " not set").c_str();
-}
-
 class ParameterBox : public std::vector<AbstractParameter>
 {
 public:
