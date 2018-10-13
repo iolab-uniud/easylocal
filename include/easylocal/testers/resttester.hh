64314c6bbe59959290ea02672b484006a4f867ff
Luca Di Gaspero <l.digaspero@uniud.it>
2018-10-13 08:10:42 +0200

    Working on RESTTester, currently a stub is ready.

--- include/easylocal/testers/resttester.hh
+++ include/easylocal/testers/resttester.hh
@@ -1,21 +1,44 @@
 #pragma once
 
-#include "../../../crow/crow_all.h"
+#include "easylocal/utils/crow_all.h"
 #include <memory>
+#include <type_traits>
 
 namespace EasyLocal {
   namespace Debug {
     
+    /** A REST Runner Tester handles a single runner
+     @ingroup Testers
+     */
+    template <class Runner>
+    class RESTRunnerTester
+    {
+      typedef typename Runner::CostStructure::CFtype CFtype;
+      typedef typename Runner::Input Input;
+      typedef typename Runner::State State;
+      typedef typename Runner::Move Move;
+    public:
+      RESTRunnerTester() {}
+    };
+    
     /** A REST Tester represents the web service interface of a easylocal solver. Differently from the regular tester, this class is State-less (w.r.t. easylocal state)
      @ingroup Testers
      */
-    template <class Input, class Output, class State, class CostStructure, class StateManager, class OutputManager>
+    template <class StateManager, class OutputManager>
     class RESTTester
     {
-      typedef typename CostStructure::CFtype CFtype;
+      typedef typename StateManager::CostStructure::CFtype CFtype;
+      typedef typename StateManager::Input Input;
+      typedef typename StateManager::State State;
+      typedef typename OutputManager::Output Output;
+      typedef typename StateManager::CostStructure CostStructure;
       
     public:
-      RESTTester() {}
+      RESTTester()
+      {
+        static_assert(std::is_same<typename StateManager::Input, typename OutputManager::Input>::value, "StateManager and OutputManager have different Input");
+        static_assert(std::is_same<typename StateManager::State, typename OutputManager::State>::value, "StateManager and OutputManager have different State");
+      }
       /** Virtual destructor. */
       virtual ~RESTTester() {}
       //  void AddMoveTester(MoveTester<Input, Output, State, CostStructure> &amt);
@@ -25,22 +48,12 @@ namespace EasyLocal {
     protected:
       //  std::vector<RunnerTester<Input, Output, State, CostStructure>*> runner_testers;                                         /**< The output object. */
       
-      //  crow::SimpleApp app;
+      crow::SimpleApp app;
     };
     
-    /** Factory method to create a rest_tester providing a state and an output manager.
-        Note: To be “strictly” used with  auto type (see the very involved signature) */
     template <class StateManager, class OutputManager>
-    std::unique_ptr<RESTTester<typename StateManager::Input, typename OutputManager::Output, typename StateManager::State, typename StateManager::CostStructure, StateManager, OutputManager>> create_rest_tester()
+    void RESTTester<StateManager, OutputManager>::Run()
     {
-      return std::unique_ptr<RESTTester<typename StateManager::Input, typename OutputManager::Output, typename StateManager::State, typename StateManager::CostStructure, StateManager, OutputManager>>(new RESTTester<typename StateManager::Input, typename OutputManager::Output, typename StateManager::State, typename StateManager::CostStructure, StateManager, OutputManager>());
-    }
-    
-    template <class Input, class Output, class State, class CostStructure, class StateManager, class OutputManager>
-    void RESTTester<Input, Output, State, CostStructure, StateManager, OutputManager>::Run()
-    {
-      crow::SimpleApp app;
-      
       CROW_ROUTE(app, "/")([](){
         return "Hello world";
       });
