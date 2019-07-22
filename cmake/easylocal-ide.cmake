4a9b479fae04d5b752dbe78fdaac4fa6683a6c97
Luca Di Gaspero <l.digaspero@uniud.it>
2019-07-22 17:05:20 +0200

    Added checking for stoi function.

--- cmake/easylocal-ide.cmake
+++ cmake/easylocal-ide.cmake
@@ -1,3 +1,5 @@
+include(CheckCXXSymbolExists)
+
 set(subdirs helpers observers runners solvers testers utils modeling)
 foreach (subdir ${subdirs})
 	string(SUBSTRING ${subdir} 0 1 FIRST_LETTER)
@@ -9,4 +11,8 @@ endforeach (subdir)
 
 source_group("EasyLocal" REGULAR_EXPRESSION "[Ee]asy[Ll]ocal.*/include/[^/]+\.hh$")
 
+CHECK_CXX_SYMBOL_EXISTS(std::stoi string HAVE_STD_STOI)
+if (HAVE_STD_STOI)
+  add_compile_definitions(HAVE_STD_STOI)
+endif (HAVE_STD_STOI)
 #source_group("EasyLocal" REGULAR_EXPRESSION "[Ee]asy[Ll]ocal.*/.+\.hh$")
